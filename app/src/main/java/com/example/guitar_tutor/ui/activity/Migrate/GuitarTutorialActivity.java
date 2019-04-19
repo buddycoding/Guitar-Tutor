5dd096f274b54c6a2764f0309568d4eda2c793f4
buddycoding <dummyemail12313123@gmail.com>
2019-04-19 18:54:15 +0800

    added new chords

--- app/src/main/java/com/example/guitar_tutor/ui/activity/Migrate/GuitarTutorialActivity.java
+++ app/src/main/java/com/example/guitar_tutor/ui/activity/Migrate/GuitarTutorialActivity.java
@@ -1,5 +1,6 @@
 package com.example.guitar_tutor.ui.activity.Migrate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,6 +12,8 @@ import com.example.guitar_tutor.R;
 public class GuitarTutorialActivity extends AppCompatActivity {
 
     private Button button1;
+    private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +27,31 @@ public class GuitarTutorialActivity extends AppCompatActivity {
                 //openMainActivity();
             }
         });
+
+        button2 = findViewById(R.id.button8);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGuitarParts();
+            }
+        });
+
+        button3 = findViewById(R.id.button10);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGuitarStrings();
+            }
+        });
+    }
+
+    public void openGuitarParts () {
+        Intent intent = new Intent (this, TutorialGuitarParts.class);
+        startActivity(intent);
+    }
+
+    public void openGuitarStrings () {
+        Intent intent = new Intent (this, TutorialGuitarString.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/guitar_tutor/ui/activity/Migrate/TutorialGuitarString.java
@@ -0,0 +1,15 @@
+package com.example.guitar_tutor.ui.activity.Migrate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.guitar_tutor.R;
+
+public class TutorialGuitarString extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial_guitar_string);
+    }
+}
